fae0238110a0f8e9ba07200eaabe851f5866dfc2
toleman84 <gdponcemarsiglia@gmail.com>
2024-04-10 14:25:13 -0300

    add exercise nº1

--- ls/0.c
+++ ls/0.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 /**
  * main - 0. Let's start with something simple!
- * @void: that lists the content of the current directory.
+ * @path: path
  *
  * Return: Alwais 0 (Success)
  */
 
-int main(void)
+void list_directory(const char *path)
 {
 	DIR *dir;
 	struct dirent *entry;
 
 	/*Open current directory*/
-	dir = opendir("./");
+	dir = opendir(path);
+	if (dir == NULL)
+	{
+		fprintf(stderr, "%s: %s %s: %s\n", "./hls", "cannot access", path, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
 
 	/*Read directory entries*/
 	while ((entry = readdir(dir)) != NULL)
@@ -26,6 +34,34 @@ int main(void)
 
 	/*Close directory*/
 	closedir(dir);
+}
 
+/**
+ * main - 1. Maybe some parameters?
+ * @argc: counter
+ * @argv: vector
+ *
+ * Return: Alwais 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	/* If no arguments provided, list current directory */
+	if (argc == 1)
+	{
+		list_directory("./");
+	} else {
+		/* Iterate through command-line arguments */
+		for (i = 1; i < argc; i++) {
+			printf("%s:\n", argv[i]);
+			list_directory(argv[i]);
+			/*loop for counter an argc: if c == counter so printf*/
+			if (argc > 2) {
+				printf("\n");
+			}
+		}
+	}
 	return (0);
 }
