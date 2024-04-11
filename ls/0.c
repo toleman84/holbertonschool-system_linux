ad41f34890f33649fea65843660c426bb31c755c
toleman84 <gdponcemarsiglia@gmail.com>
2024-04-11 03:42:14 -0300

    fix

--- ls/0.c
+++ ls/0.c
@@ -5,25 +5,18 @@
 #include <string.h>
 
 /**
- * list_directory - 1. Maybe some parameters?
- * @path: path
+ * main - 1. Maybe some parameters?
  *
  * Return: Alwais 0 (Success)
  */
 
-void list_directory(const char *path)
+int main(void)
 {
 	DIR *dir;
 	struct dirent *entry;
 
 	/*Open current directory*/
-	dir = opendir(path);
-	if (dir == NULL)
-	{
-		fprintf(stderr, "%s: %s %s: %s\n", "./hls", "cannot access", path,\
-			strerror(errno));
-		exit(EXIT_FAILURE);
-	}
+	dir = opendir("./");
 
 	/*Read directory entries*/
 	while ((entry = readdir(dir)) != NULL)
@@ -35,37 +28,6 @@ void list_directory(const char *path)
 
 	/*Close directory*/
 	closedir(dir);
-}
 
-/**
- * main - 1. Maybe some parameters?
- * @argc: counter
- * @argv: vector
- *
- * Return: Alwais 0 (Success)
- */
-
-int main(int argc, char *argv[])
-{
-	int i;
-
-	/* If no arguments provided, list current directory */
-	if (argc == 1)
-	{
-		list_directory("./");
-	} else
-	{
-		/* Iterate through command-line arguments */
-		for (i = 1; i < argc; i++)
-		{
-			printf("%s:\n", argv[i]);
-			list_directory(argv[i]);
-			/*loop for counter an argc: if c == counter so printf*/
-			if (argc > 2)
-			{
-				printf("\n");
-			}
-		}
-	}
 	return (0);
 }
